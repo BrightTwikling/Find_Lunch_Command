5e497da6a7c5dd7a6022a8cd473c2622d5483e12
BrightTwikling <DF.SJbbba5@gmail.com>
2024-11-11 20:31:18 +0900

    Translate comment to English
    Signed-off-by: BrightTwikling <DF.SJbbba5@gmail.com>

--- convert_dependencies_to_xml.sh
+++ convert_dependencies_to_xml.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# JSONファイルのパス
+# Path of the .dependencies file
 json_file=$1
 
-# JSONオブジェクトの数を取得
+# Get the number of objects in .dependencies
 num_objects=$(jq '. | length' "$json_file")
 
-# 各JSONオブジェクトをパースして変換
+# Parse and transform each object
 for (( i=0; i<$num_objects; i++ )); do
     remote=$(jq -r ".[$i].remote" "$json_file")
     repository=$(jq -r ".[$i].repository" "$json_file")
     target_path=$(jq -r ".[$i].target_path" "$json_file")
     revision=$(jq -r ".[$i].revision" "$json_file")
 
-    # repositoryの代わりにtarget_pathを使用してプロジェクト名を生成
+    # Generate project name using target_path instead of repository
     project_name=$target_path
 
-    # 結果を一行に変換
+    # Convert the result to a single line
     echo "  <project path=\"$target_path\" name=\"$repository\" remote=\"$remote\" revision=\"$revision\" />"
 done
